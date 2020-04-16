71853deef69bfc4bcfdb4405a4980a50baddff71
Troy Murray <z5118366@ad.unsw.edu.au>
2020-04-16 22:27:00 +1000

    implemented as_activate and as_deactivate, copied from dumb_vm

--- kern/vm/addrspace.c
+++ kern/vm/addrspace.c
@@ -95,33 +95,34 @@ as_destroy(struct addrspace *as)
 	kfree(as);
 }
 
+
+// copied from dumbvm.c
 void
 as_activate(void)
 {
+	int i, spl;
 	struct addrspace *as;
 
 	as = proc_getas();
 	if (as == NULL) {
-		/*
-		 * Kernel thread without an address space; leave the
-		 * prior address space in place.
-		 */
 		return;
 	}
 
-	/*
-	 * Write this.
-	 */
+	/* Disable interrupts on this CPU while frobbing the TLB. */
+	spl = splhigh();
+
+	for (i=0; i<NUM_TLB; i++) {
+		tlb_write(TLBHI_INVALID(i), TLBLO_INVALID(), i);
+	}
+
+	splx(spl);
 }
 
+// copied from dumbvm.c
 void
 as_deactivate(void)
 {
-	/*
-	 * Write this. For many designs it won't need to actually do
-	 * anything. See proc.c for an explanation of why it (might)
-	 * be needed.
-	 */
+	/* nothing */
 }
 
 /*
